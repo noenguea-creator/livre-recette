1fadcc4745355e0a0e395cd36cb799114f76ab9f
noenguea-creator <noe.nguea@gmail.com>
2025-12-09 11:55:12 +0100

    ferv

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+/**
+ * role affiche la recette de cuisine en console 
+ * parametre: recette la recette a afficher en console 
+ * retour rien
+ */
+function afficheEnConsole(recette){
+    console.log(`%c${recette.nom}`,"color:red; font-size:42px; text-transform:uppercase")
+    console.log(`Diffculté : ${recette.difficulte}`)
+    console.log(`Temps de préparation : ${recette.tempPreparation}`)
+    console.log(`Temps de cuisson : ${recette.tempCuisson}`)
+    console.log(`Pour ${recette.nbrPortions} personnes`)
+    console.log("%cIngredients : ", "color:green; font-size: 18px; font-weight: bold")
+    recette.ingredients.forEach(ingr=>{
+        console.log(`- ${ingr}`)
+    })
+
+}
+
+
 let recettes = [
   {
     nom: "poulet rôti au thym et à l'ail",
@@ -26,14 +46,25 @@ let recettes = [
 ];
 
 
-//je veux recupere chaque recette une par une
-recettes.forEach(recette => {
-    console.log (
-        `nom : ${recette.nom}
-        tps prepa : ${recette.tempPreparation}
-        
-        `
-    )
-        
-});
+recettes.forEach(rec=>{
+    //afficheEnConsole(rec)
 
+    //je construit la liste d'ingredient
+recettes.forEach(recette=){
+  let liste=""
+  recette.ingredients.forEach(ingredient=){
+    liste}
+  }
+}
+
+    //je construit ma div 
+  document.getElementById("monid").innerHTML+=`<div>
+    <h2>${rec.nom}</h2>
+    <p>${rec.difficulte}</p>
+    <p>${rec.tempPreparation}</p>
+    <p>${rec.tempCuisson}</p>
+    <p>${rec.nbrPortions}</p>
+    <p>${}</p>
+</div>`
+    
+})
